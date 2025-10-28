6930abfa91489dd6f5f3ccb7c1fab7b66f7d659b
Kamogelo-ke <thlongkamogelo476@gmail.com>
2025-10-28 10:18:26 +0200

    reverted the changes

--- src/components/organizer_discover/discover.jsx
+++ src/components/organizer_discover/discover.jsx
@@ -76,7 +76,7 @@ const Discover = () => {
   }, [search, selectedCategory]);
 
   const handleCardClick = (id) => {
-    navigate('/attendee/view-event/${event.id}');
+    navigate(`/event/${id}`);
   };
 
   const handleShareClick = (e, eventId) => {
